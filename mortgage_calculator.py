55229379e2d8c4a5088cc551c40514e4bfdcdf34
briansukalski <sukalskibrian@gmail.com>
2021-12-02 22:34:10 -0600

    added interest rate parameter and incorporated annuity formula to calculate monthly payment

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -1,8 +1,15 @@
 class Mortgage:
-    def __init__(self, value, duration=30):
+    def __init__(self, value, interest_rate, duration=30):
         self.value = value
+        self.interest_rate = interest_rate / 100
         self.duration = duration
-        self.monthly_payment = round(value / (self.duration*12), 2)
+        self.monthly_payment = self.calculate_payment()
+    
+    def calculate_payment(self):
+        payment = self.value * (self.interest_rate /12) / (1 - (1 + self.interest_rate / 12)**(-self.duration*12))
+        payment = round(payment, 2)
+        return payment
 
-my_next_house = Mortgage(1000000)
+
+my_next_house = Mortgage(1000000, 2.5)
 print(my_next_house.monthly_payment)
